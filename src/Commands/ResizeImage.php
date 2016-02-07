fdd97615a4147892a175d910d996a6386c22549c
Jeremy Tubbs <jeremytubbs@gmail.com>
2016-02-07 17:49:41 +0000

    allow config to be passed in from outside

--- src/Commands/ResizeImage.php
+++ src/Commands/ResizeImage.php
@@ -20,17 +20,11 @@ class ResizeImage extends Job implements SelfHandling, ShouldQueue
      *
      * @return void
      */
-    public function __construct($image, $filename = null)
+    public function __construct($image, $filename = null, $config = null)
     {
         $this->image = $image;
         $this->filename = $filename;
-        $this->resizer = ResizeFactory::create([
-            'path' => config('resizer.image_path'),
-            'driver' => config('resizer.image_driver'),
-            'format' => config('resizer.image_format'),
-            'sizes' => config('resizer.image_sizes'),
-            'image2x' => config('resizer.image_2x'),
-        ]);
+        $this->setResizer($config);
     }
 
     /**
@@ -41,5 +35,16 @@ class ResizeImage extends Job implements SelfHandling, ShouldQueue
     public function handle()
     {
         $this->resizer->makeImages($this->image, $this->filename);
+    }
+
+    public function setResizer($config)
+    {
+        $this->resizer = ResizeFactory::create([
+            'path'    => isset($config['image_path']) ? $config['image_path'] : config('resizer.image_path'),
+            'driver'  => isset($config['image_driver']) ? $config['image_driver'] : config('resizer.image_driver'),
+            'format'  => isset($config['image_format']) ? $config['image_format'] : config('resizer.image_format'),
+            'sizes'   => isset($config['image_sizes']) ? $config['image_sizes'] : config('resizer.image_sizes'),
+            'image2x' => isset($config['image_2x']) ? $config['image_2x'] : config('resizer.image_2x'),
+        ]);
     }
 }
