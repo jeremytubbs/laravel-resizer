bd4ab4d7175d62f2eb54f78ca338270a6612b3f0
Jeremy Tubbs <jeremytubbs@gmail.com>
2016-03-03 12:39:19 -0500

    add picturefill.min.js and blade directives

--- src/ResizerServiceProvider.php
+++ src/ResizerServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Jeremytubbs\LaravelResizer;
 
+use Blade;
 use Illuminate\Support\ServiceProvider;
 
 class ResizerServiceProvider extends ServiceProvider
@@ -20,7 +21,24 @@ class ResizerServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Blade::directive('picturefill', function() {
+        return <<<EOF
+<script>
+    // Picture element HTML5 shiv
+    document.createElement( "picture" );
+</script>
+<script src="/vendor/laravel-resizer/picturefill.min.js" async></script>
+EOF;
+        });
+
+        Blade::directive('srcset', function($asset) {
+            $asset = trim($asset, "('..')");
+            $asset_parts = pathinfo($asset);
+            $asset_2x = $asset_parts['dirname'].'/'.$asset_parts['filename'].'@2x.'.$asset_parts['extension'];
+            return <<<EOF
+<img srcset="$asset 1x, $asset_2x 2x">
+EOF;
+        });
     }
 
         /**
@@ -33,6 +51,10 @@ class ResizerServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/../config/resizer.php' => config_path('resizer.php'),
         ], 'config');
+
+        $this->publishes([
+            __DIR__.'/assets/vendor/' => public_path('vendor/laravel-resizer'),
+        ], 'public');
 
         $this->commands([
             'Jeremytubbs\LaravelResizer\Console\Commands\ResizeImageCommand',
